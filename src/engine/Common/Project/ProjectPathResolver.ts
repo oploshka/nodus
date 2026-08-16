a873a202354b8b60e9510358692847093ea51be0
oploshka <43810171+oploshka@users.noreply.github.com>
2026-08-16 04:38:50 +0300

    refactor: remove legacy common project path resolver

--- src/engine/Common/Project/ProjectPathResolver.ts
+++
@@ -1,4 +0,0 @@
-import { PathResolver } from '@engine/Common/Tools/PathResolver.js';
-
-/** @deprecated Use PathResolver from Common/Tools. */
-export class ProjectPathResolver extends PathResolver {}
