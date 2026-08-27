af96433acbf188ebd9f9ce61c8092f6d733ac8e8
oploshka <43810171+oploshka@users.noreply.github.com>
2026-08-28 01:49:14 +0300

    Register planner response definition

--- automation/index.js
+++ automation/index.js
@@ -2,6 +2,7 @@ import codeChange from './schemas/code-change.js';
 import defaultPlanner from './planners/default.js';
 import codeWorker from './workers/code.js';
 import changeCodeResponse from './responses/change-code.js';
+import plannerResponse from './responses/planner.js';
 
 export default {
   prompts: {
@@ -18,6 +19,7 @@ export default {
     code: codeWorker,
   },
   responses: {
+    planner: plannerResponse,
     'change-code': changeCodeResponse,
   },
 };
