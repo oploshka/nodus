3763b3b6f8303c6424638c85d2f869eba1d79892
oploshka <43810171+oploshka@users.noreply.github.com>
2026-08-30 22:06:57 +0300

    Register automation Worker classes

--- automation/index.js
+++ automation/index.js
@@ -1,6 +1,8 @@
 import PlannerTask from '#automation/Planner/PlannerTask/PlannerTask.js';
 import QualifierTask from '#automation/Qualifier/QualifierTask/QualifierTask.js';
+import WorkerAgent from '#automation/Worker/WorkerAgent/WorkerAgent.js';
 import WorkerCode from '#automation/Worker/WorkerCode/WorkerCode.js';
+import WorkerDocumentation from '#automation/Worker/WorkerDocumentation/WorkerDocumentation.js';
 
 export default {
   planners: {
@@ -10,6 +12,8 @@ export default {
     task: QualifierTask,
   },
   workers: {
+    agent: WorkerAgent,
     code: WorkerCode,
+    documentation: WorkerDocumentation,
   },
 };
