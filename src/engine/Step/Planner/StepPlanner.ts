fda5d50e0d22ebb38df5b6b2473ec75a8d80f8d8
oploshka <43810171+oploshka@users.noreply.github.com>
2026-08-31 19:51:33 +0300

    Use getGroup in StepPlanner

--- src/engine/Step/Planner/StepPlanner.ts
+++ src/engine/Step/Planner/StepPlanner.ts
@@ -2,5 +2,7 @@ import { EngineStep } from '@engine/Core/EngineStep.js';
 
 /** Base class for planner Steps. */
 export abstract class StepPlanner extends EngineStep {
-  public readonly group = 'planner';
+  public getGroup(): string {
+    return 'planner';
+  }
 }
