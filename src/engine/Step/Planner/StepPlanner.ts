7a57c09685d56fdcf203ebafbd446b3b2303ebb0
oploshka <43810171+oploshka@users.noreply.github.com>
2026-08-31 20:05:55 +0300

    Remove nested StepPlanner

--- src/engine/Step/Planner/StepPlanner.ts
+++
@@ -1,8 +0,0 @@
-import { EngineStep } from '@engine/Core/EngineStep.js';
-
-/** Base class for planner Steps. */
-export abstract class StepPlanner extends EngineStep {
-  public getGroup(): string {
-    return 'planner';
-  }
-}
