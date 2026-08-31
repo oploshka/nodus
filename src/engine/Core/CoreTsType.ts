20427f26442685cfe7cace1d85aab29c42fae2ab
oploshka <43810171+oploshka@users.noreply.github.com>
2026-08-31 17:27:15 +0300

    Keep Engine configuration outside Core types

--- src/engine/Core/CoreTsType.ts
+++ src/engine/Core/CoreTsType.ts
@@ -50,11 +50,6 @@ export interface sCoreGroupConfig {
   readonly schema: false | sCoreGroupSchemaConfig;
 }
 
-export interface sCoreConfig {
-  readonly groups: Readonly<Record<string, sCoreGroupConfig>>;
-  readonly modules: Readonly<Record<string, tCoreModuleDefinition>>;
-}
-
 export interface sCoreTraceEntry {
   path: readonly number[];
   module?: string;
