a6048e094539d8fba30faad5634a1415c8ece75b
oploshka <43810171+oploshka@users.noreply.github.com>
2026-08-31 19:26:48 +0300

    Remove split worker contract

--- src/engine/Step/Worker/Contract/WorkerTsType.ts
+++
@@ -1,6 +0,0 @@
-import type { sCoreModuleRequest } from '@engine/Core/CoreTsType.js';
-import type { sCoreOutput, sCoreSequence } from '@engine/Core/CoreSchema.js';
-
-export interface sWorkerRequest extends sCoreModuleRequest {}
-export interface sWorkerOutput extends sCoreOutput {}
-export interface sWorkerSchema extends sCoreSequence {}
