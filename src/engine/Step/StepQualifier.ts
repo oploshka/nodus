33492c2cb2277f5297da9276bc03c5dad845e700
oploshka <43810171+oploshka@users.noreply.github.com>
2026-09-01 02:51:51 +0300

    Add Qualifier Step metadata

--- src/engine/Step/StepQualifier.ts
+++ src/engine/Step/StepQualifier.ts
@@ -1,7 +1,12 @@
 import { EngineStep } from '@engine/Core/EngineStep.js';
+import type { sEngineStepMetadata } from '@engine/Core/EngineStepInterface.js';
 
 export abstract class StepQualifier extends EngineStep {
   public getGroup(): string {
     return 'qualifier';
+  }
+
+  public getMetadata(): sEngineStepMetadata {
+    return { ...super.getMetadata(), title: 'Qualifier', color: 'cyan' };
   }
 }
