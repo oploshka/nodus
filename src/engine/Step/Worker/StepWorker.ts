adbab014ff38c5b4d1a1139b6be17f7f39bf59ad
oploshka <43810171+oploshka@users.noreply.github.com>
2026-08-31 20:06:03 +0300

    Remove nested StepWorker

--- src/engine/Step/Worker/StepWorker.ts
+++
@@ -1,8 +0,0 @@
-import { EngineStep } from '@engine/Core/EngineStep.js';
-
-/** Base class for worker Steps. */
-export abstract class StepWorker extends EngineStep {
-  public getGroup(): string {
-    return 'worker';
-  }
-}
