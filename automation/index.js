cb532b2c70097a9abe9d10ea3be981d5e0c64d73
oploshka <43810171+oploshka@users.noreply.github.com>
2026-09-08 16:56:58 +0300

    Expose automation root Step

--- automation/index.js
+++ automation/index.js
@@ -1,30 +1,5 @@
-import { Planner } from '#automation/Step/Planner/Planner.ts';
-import WorkerCode from '#automation/Step/Worker/WorkerCode/WorkerCode.ts';
+import { QualificationTask } from '#automation/Step/Qualification/QualificationTask.ts';
 
 export default {
-  groups: {
-    planner: {
-      schema: {
-        allowedGroups: ['worker', 'research'],
-      },
-    },
-    worker: {
-      schema: {
-        allowedGroups: ['action', 'research'],
-      },
-    },
-    research: {
-      schema: {
-        allowedGroups: ['action'],
-      },
-    },
-    action: {
-      schema: false,
-    },
-  },
-
-  modules: {
-    Planner: new Planner(),
-    WorkerCode: new WorkerCode(),
-  },
+  root: new QualificationTask(),
 };
